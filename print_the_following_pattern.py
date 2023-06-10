1a65f5bde42ded75c44245d8c550a4d40856d053
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-06-10 15:06:45 +0530

    def print_pattern(n):     for i in range(1, n+1):         if i == n or i == 1:             print('*' * i)         else:             print('*' + ' ' * (i-2) + '*') n=int(input()) print_pattern(n)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,8 @@
 def print_pattern(n):
-    for i in range(n, 0, -1):
-        for j in range(1, i+1):
-            print(j, end="")
-        print()
-
+    for i in range(1, n+1):
+        if i == n or i == 1:
+            print('*' * i)
+        else:
+            print('*' + ' ' * (i-2) + '*')
 n=int(input())
 print_pattern(n)
